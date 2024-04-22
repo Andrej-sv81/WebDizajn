a1a3846de67ed2b1388805ea326c7aabbe5261bb
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2024-04-22 18:48:06 +0200

    Components
    -required components
    -home component layout and navbar

---
+++ one-for-all/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+
+images = [
+    { url: 'https://i.imgur.com/OV15WM6.jpeg', name: 'BeoArt Fest' },
+    { url: 'https://i.imgur.com/jsFrKCn.jpeg', name: 'Etno Srbija' },
+    { url: 'https://i.imgur.com/NhAzBBF.jpeg', name: 'Lazarevac Fest' },
+    { url: 'https://i.imgur.com/k4nEZm6.jpeg', name: 'Dunavski Horizonti' },
+    { url: 'https://i.imgur.com/nmAOOHF.jpeg', name: 'Urban Beat' },
+];
+constructor(private router: Router){};
+
+imageClick() {
+  this.router.navigate(['']);
+}
+
+ngOnInit(): void {
+}
+
+}
+
